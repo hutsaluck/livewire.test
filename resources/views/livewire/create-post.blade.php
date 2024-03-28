b7de7451e000c875a4525dce7483735324f40ac7
Юра Гуцалюк <hutsaluck@gmail.com>
2024-03-28 12:08:35 +0200

    Change style form

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -5,22 +5,22 @@
 
     <form method="POST" wire:submit="save">
     <div>
-            <label for="title" class="block font-medium text-sm text-gray-700">Title</label>
-            <input id="title" wire:model="title" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm" type="text" />
+            <label for="title" class="block font-medium text-sm text-white">Title</label>
+            <input id="title" wire:model="title" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm text-gray-700" type="text" />
             @error('title')
             <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
             @enderror
         </div>
 
         <div class="mt-4">
-            <label for="body" class="block font-medium text-sm text-gray-700">Body</label>
-            <textarea id="body" wire:model="body" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm"></textarea>
+            <label for="body" class="block font-medium text-sm text-white">Body</label>
+            <textarea id="body" wire:model="body" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm text-gray-700"></textarea>
             @error('body')
             <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
             @enderror
         </div>
 
-        <button class="mt-4 px-4 py-2 bg-gray-800 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
+        <button class="border border-white mt-4 px-4 py-2 bg-gray-800 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
             Save
         </button>
     </form>
